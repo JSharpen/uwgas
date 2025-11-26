541ea8742b6e484bf7a917b9de6952a0c1acbd82
JSharpen <jordan.carruthers@gmail.com>
2025-11-26 21:14:06 +1100

    Update vite.config.ts

--- angle-setter/vite.config.ts
+++ angle-setter/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/uwgas/angle-setter/',   // 👈 this is for GitHub Pages URL
+  base: '/uwgas/',   // 👈 this is for GitHub Pages URL
   plugins: [react()],
 })
